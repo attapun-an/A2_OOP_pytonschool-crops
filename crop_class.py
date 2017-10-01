05cf8774a4c876f08e992f93ef5848eb9198fe04
attapun-an <attapunanivat@gmail.com>
2017-10-01 23:20:38 +0700

    starting methods
    Adding methods to our class
    In the previous section you were introduced to the concept of an
    interface - a way of interacting an object in a predefined way. The
    example used was that of a television remote control, the interface it
    provides is a set of buttons that you can press to change channels and
    access other functionality. Behind the scenes much more is happening
    but you interact with the television through this interface. The same
    is true with any classes you define, you will write methods that you
    and others can use to interact with objects of that type.
    
    We are going to add two methods to our class:
    
    Needs
    Report

--- crop_class.py
+++ crop_class.py
@@ -1,5 +1,6 @@
 class Crop:
     """A generic food crop"""
+
     # the line above is a doc string (gives python info about what the class does)
 
     # constructor (method that runs automatically to instantiate the class)
@@ -18,12 +19,44 @@ class Crop:
         # attributes with ._ are considered private (can only be accessed by methods)
         # which means they can't be accessed directly from outside the class
 
+        """ if it's something that can generally be changed - then it's okay being 
+        public, but if it's something that your methods deal with anyways have it private...
+        
+        If someone just came in, and changed your height .. 
+        but someone can change your diet, and how much food you eat - Mr. M 
+        
+        Name, Favourite color can be changed
+        But you can't just say tommorrow I will be 2ft taller"""
+
+    def display_status(self):
+        return {"growth": self._growth, "days_growing": self._days_growing}
+
+    def needs(self):
+        return{'light need': self._light_need, 'water need': self._light_need}
+
+    def report(self):
+        return
+
+
 def main():
     # instantiate the class
-    new_crop = Crop(10,5,5)
+    new_crop = Crop(10, 5, 5)
     # lets see if it works
     print(new_crop._status)
     """whoa, we just accessed a private attribute! as a programmer, it is okay for testing
     but once we compile it - that's when it truly becomes private"""
+    print(new_crop._days_growing)
+    print(new_crop._growth_rate)
+    print(new_crop._light_need)
+    print(new_crop._water_need)
 
-main()
+    new_crop2 = Crop(1, 2 , 4)
+    print(new_crop2._days_growing)
+    print(new_crop2._growth_rate)
+    print(new_crop2._light_need)
+    print(new_crop2._water_need)
+
+
+# no idea how this is different from just main(). but it looks cool
+if __name__ == "__main__":
+    main()
