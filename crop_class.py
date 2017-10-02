38f1a3a13a8b64c5e0612a2cfd5cd7012b01298b
attapun-an <attapunanivat@gmail.com>
2017-10-02 22:01:06 +0700

    hope this doesn't error

--- crop_class.py
+++ crop_class.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Crop:
     """A generic food crop"""
 
@@ -31,16 +34,61 @@ class Crop:
     def display_status(self):
         return {"growth": self._growth, "days_growing": self._days_growing}
 
+    # returns amount of light and water that crop requires to grow
     def needs(self):
+        # returns a dictionary with light and water needs. Yay.
         return{'light need': self._light_need, 'water need': self._light_need}
 
+    # provides overview of current state
     def report(self):
-        return
+        # returns a dictionary containing type, status, growth, days growing
+        return{'type': self._type, 'status': self._status, 'growth': self._growth, 'days growing': self._days_growing}
+
+    def _update_status(self):
+        current_growth = self._days_growing
+        if current_growth > 15:
+            self._status = "old"
+        elif current_growth > 10:
+            self._status = "mature"
+        elif current_growth > 5:
+            self._status = "young"
+        elif current_growth > 0:
+            self._status = "seedling"
+        elif current_growth == 0:
+            self._status = "seed"
+
+    def grow(self, light, water):
+        if light >= self._light_need and water >= self._water_need:
+            self._growth += self._growth_rate
+        # increment days growing
+        self._days_growing += 1
+        # update status
+        self._update_status()
+
+
+def auto_grow(crop, days):
+    for days in range (days):
+        light = random.randint(1, 10)
+        water = random.randint(1, 10)
+        crop.grow(light, water)
+
+
+def manual_grow(crop):
+    # get light and water values
+    light_check = True
+    while light_check:
+        try:
+            light_check = int(input("light (1-10): ")):
+            if light_check > 0 and light_check < 10:
+        except ValueError:
+            print("Invalid")
+
+
 
 
 def main():
     # instantiate the class
-    new_crop = Crop(10, 5, 5)
+    new_crop = Crop(2, 5, 5)
     # lets see if it works
     print(new_crop._status)
     """whoa, we just accessed a private attribute! as a programmer, it is okay for testing
@@ -50,12 +98,25 @@ def main():
     print(new_crop._light_need)
     print(new_crop._water_need)
 
+    """
     new_crop2 = Crop(1, 2 , 4)
     print(new_crop2._days_growing)
     print(new_crop2._growth_rate)
     print(new_crop2._light_need)
     print(new_crop2._water_need)
+    """
 
+    print(new_crop.needs())
+    print(new_crop.report())
+
+    #printing only a portion
+    print(new_crop.needs()['light need'])
+
+    new_crop.grow(4, 4)
+    print(new_crop.report())
+
+    auto_grow(new_crop, 30)
+    print(new_crop.report())
 
 # no idea how this is different from just main(). but it looks cool
 if __name__ == "__main__":
