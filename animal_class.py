7398bb4d2df80fea060db2574e357dc6bdaab652
attapun-an <attapunanivat@gmail.com>
2017-10-06 08:29:47 +0700

    lets add animals
    new animal class in progress

---
+++ animal_class.py
@@ -0,0 +1,14 @@
+
+class Animal():
+
+    # constructor
+    def __init__(self, food_need, water_need):
+        weight = pass
+        self._days_growing = 0
+        self._growth_rate = 1
+        self._food_need = food_need
+        self._water_need = water_need
+        self._status = "new-born"
+        self._type = "normal"
+        self._name = "name"
+
